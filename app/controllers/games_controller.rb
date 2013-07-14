d389a6af45c72d25c60b4c30cac3fa464ccf2eea
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-14 18:56:14 -0400

    Added soundcloud player to each level

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -25,6 +25,7 @@ class GamesController < ApplicationController
   end
 
   def level
+    client = Soundcloud.new(:client_id => 'fd18da9af13744f5233916b5025adf02')
     @id = params[:id].to_i
     @track = $tracks_with_genre[@id]
     @track_genre = @track.genre.capitalize
@@ -33,6 +34,8 @@ class GamesController < ApplicationController
     @genre_list.shuffle!
     @genre_list = @genre_list[0..2]
 
+    embed_info = client.get('/oembed', :url => @track.uri)
+    @widget = embed_info['html']
 
     @points = 10
   end
