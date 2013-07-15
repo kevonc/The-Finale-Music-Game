93c04520ade9298e182a1988d78103cb7ebc3886
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-15 11:57:15 -0400

    Refactored code to eliminate start action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,34 +2,19 @@ class GamesController < ApplicationController
   def index
   end
 
-  def start
-    client = Soundcloud.new(:client_id => 'fd18da9af13744f5233916b5025adf02')
-    @tracks = client.get('/tracks', :limit => 100, :order => 'created_at')
-    @tracks.shuffle!
-
-    # get rid of tracks that have no genre stated
-    $tracks_with_genre = []
-    @tracks.each do |track|
-      if track.genre && track.genre != '' && track.uri != ''
-        $tracks_with_genre << track
-      end
-    end
-
-
-    @genre_list = Genre.all
-    @genre_list.shuffle!
-    @genre_list = @genre_list[0..2]
-
-    @track = $tracks_with_genre.first
-    @track_genre = @track.genre.capitalize
-
-    embed_info = client.get('/oembed', :url => @track.uri)
-    @widget = embed_info['html']
-  end
-
   def level
     client = Soundcloud.new(:client_id => 'fd18da9af13744f5233916b5025adf02')
     @id = params[:id].to_i
+    if @id == 1
+      $tracks_with_genre = []
+      @tracks = client.get('/tracks', :limit => 100, :order => 'created_at')
+      @tracks.each do |track|
+        if track.genre && track.genre != '' && track.uri != ''
+          $tracks_with_genre << track
+        end
+      end
+    end
+
     @track = $tracks_with_genre[@id]
     @track_genre = @track.genre.capitalize
 
