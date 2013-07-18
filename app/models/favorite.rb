6490d39c2733f81d9792cf0590a48eba2306a17d
Kevon Cheung <kevon.cheung@gmail.com>
2013-07-18 14:40:47 -0400

    Edited about page and buttons for signup and login

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,6 @@
 class Favorite < ActiveRecord::Base
   attr_accessible :track, :url, :user_id, :genre, :uploaded_by, :created, :artwork, :soundcloud_id
   validates :soundcloud_id, uniqueness: true
+
+  default_scope order('id DESC')
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,5 +2,5 @@ class Game < ActiveRecord::Base
   attr_accessible :points, :user_id
   belongs_to :user
 
-
+  default_scope order('id ASC')
 end
